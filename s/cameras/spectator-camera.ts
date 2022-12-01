9d595cf0f19d30212481ec299203a2a3b07cd64a
Paul_Aroo <timmypaul2006@gmail.com>
2022-12-01 16:30:55 +0100

    wip: optimize gravity and collisions for camera

--- s/cameras/spectator-camera.ts
+++ s/cameras/spectator-camera.ts
@@ -1,14 +1,19 @@
 
-import {Scene} from "@babylonjs/core/scene.js"
+import {Theater} from "../theater/theater.js"
 import {Vector3} from "@babylonjs/core/Maths/math.vector.js"
 import {UniversalCamera} from "@babylonjs/core/Cameras/universalCamera.js"
 
-export default function makeSpectatorCamera(scene: Scene) {
+export default function makeSpectatorCamera({
+		theater, sampleHeight
+	}: {
+		theater: Theater
+		sampleHeight: (x: number, y: number) => number
+	}) {
 
 	const camera = (() => {
 		const name = "cammm"
 		const position = new Vector3(0, 0, 0)
-		return new UniversalCamera(name, position, scene)
+		return new UniversalCamera(name, position, theater.scene)
 	})()
 
 	camera.attachControl()
@@ -20,6 +25,36 @@ export default function makeSpectatorCamera(scene: Scene) {
 	camera.keysLeft.push(65)
 	camera.keysDown.push(83)
 	camera.keysRight.push(68)
+
+	let targetHeight = 0
+	const minimumHeightOffGround = 10
+
+	function updateTargetHeight() {
+		targetHeight = sampleHeight(
+			camera.globalPosition.x,
+			camera.globalPosition.z,
+		)
+	}
+
+	function smoothUpdateForCameraHeight() {
+		const currentHeight = camera.position.y
+		const difference = targetHeight - currentHeight
+		const fractionOfDifference = difference * 0.1
+		camera.position.y += fractionOfDifference
+		const floor = sampleHeight(
+			camera.globalPosition.x,
+			camera.globalPosition.z,
+		) + minimumHeightOffGround
+		if (camera.globalPosition.y < floor) {
+			const underneath = floor - camera.globalPosition.y
+			camera.position.y += underneath
+		}
+	}
+
+	theater.renderLoop.add(() => {
+		updateTargetHeight()
+		smoothUpdateForCameraHeight()
+	})
 
 	return camera
 }
