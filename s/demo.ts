ae8ddf596c44d893396d86857e897cad886fac6b
zenkyu <przemekgg2002@gmail.com>
2023-01-23 05:07:16 +0100

    add: boundary, workload sliders

--- s/demo.ts
+++ s/demo.ts
@@ -3,21 +3,32 @@ import {makeTerrarium} from "./main.js"
 import {getElements, registerElements} from "@benev/toolbox/x/main.js"
 
 registerElements(getElements())
-const world = makeTerrarium()
 
-document.body.appendChild(world.slider)
+const world = makeTerrarium()
+const wrapper = document.createElement("div")
+wrapper.classList.add("wrapper")
+wrapper.style.display = "flex"
+const firstColumn = document.createElement("div")
+const secondColumn = document.createElement("div")
 document.body.appendChild(world.theater.canvas)
-document.body.appendChild(world.settings.element)
+document.body.appendChild(wrapper)
 document.body.appendChild(world.slider)
-document.body.appendChild(world.frameRateDisplay)
-document.body.appendChild(world.cameraPosDisplay)
-document.body.appendChild(world.gpuFrameTimeCounter)
-document.body.appendChild(world.frameTimeCounter)
-document.body.appendChild(world.drawTimeCounter)
-document.body.appendChild(world.interFrameCounter)
-document.body.appendChild(world.cameraRenderTimeCounter)
-document.body.appendChild(world.activeMeshesEvaluationTimeCounter)
-;(<any>window).theater = world.theater
+
+wrapper.appendChild(firstColumn)
+wrapper.appendChild(secondColumn)
+
+secondColumn.appendChild(world.levelOfDetailSlider)
+secondColumn.appendChild(world.boundarySlider)
+secondColumn.appendChild(world.workloadBudgetSlider)
+firstColumn.appendChild(world.settings.element)
+firstColumn.appendChild(world.frameRateDisplay)
+firstColumn.appendChild(world.cameraPosDisplay)
+firstColumn.appendChild(world.gpuFrameTimeCounter)
+firstColumn.appendChild(world.frameTimeCounter)
+firstColumn.appendChild(world.drawTimeCounter)
+firstColumn.appendChild(world.interFrameCounter)
+firstColumn.appendChild(world.cameraRenderTimeCounter)
+firstColumn.appendChild(world.activeMeshesEvaluationTimeCounter)
 ;(<any>window).theater = world.theater
 
 world.initialize()
--- s/editor-ui/make-slider.ts
+++ s/editor-ui/make-slider.ts
@@ -1,10 +1,26 @@
 
-import {RangeSlider} from "@benev/toolbox/x/editor-ui/range-slider/element.js"
+import {getElements, registerElements} from "@benev/toolbox"
 
-export function makeSlider() {
+export function makeSliders(boundary: number, levelOfDetailValue: number, workloadBudgetValue: number) {
 
-	const slider = document.createElement("range-slider")
-	slider.setAttribute("label", "Rangle slider")
-	slider.setAttribute("step", "1")
-	return slider
+	const levelOfDetailSlider = document.createElement("range-slider")
+	levelOfDetailSlider.setAttribute("label", "Level of detail")
+	levelOfDetailSlider.setAttribute("initial-value", `${levelOfDetailValue}`)
+	levelOfDetailSlider.setAttribute("step", "1")
+
+	const workloadBudgetSlider = document.createElement("range-slider")
+	workloadBudgetSlider.setAttribute("label", "Workload budget")
+	workloadBudgetSlider.setAttribute("initial-value", `${workloadBudgetValue}`)
+	workloadBudgetSlider.setAttribute("step", "1")
+	workloadBudgetSlider.setAttribute("min", "1")
+	workloadBudgetSlider.setAttribute("max", "200")
+
+	const boundarySlider = document.createElement("range-slider")
+	boundarySlider.setAttribute("initial-value", `${boundary}`)
+	boundarySlider.setAttribute("min", `${boundary}`)
+	boundarySlider.setAttribute("max", "102400")
+	boundarySlider.setAttribute("step", `${boundary}`)
+	boundarySlider.setAttribute("label", "Boundary")
+
+	return {levelOfDetailSlider, workloadBudgetSlider, boundarySlider}
 }
