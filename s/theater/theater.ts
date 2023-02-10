adca0b202a4770ee730ec345ebd82dcf4c1cd7f6
zenkyu <przemekgg2002@gmail.com>
2023-02-10 22:00:03 +0100

    refactor: theater

--- s/theater/theater.ts
+++ s/theater/theater.ts
@@ -1,8 +1,4 @@
 
-import "@babylonjs/core/Rendering/depthRendererSceneComponent.js"
-
-import {Color4} from "@babylonjs/core/Maths/math.color.js"
-import {DepthRenderer} from "@babylonjs/core/Rendering/depthRenderer.js"
 import {BenevTheater} from "@benev/toolbox/x/babylon/theater/element.js"
 import {makeSliders} from "../editor-ui/make-slider.js"
 
@@ -15,19 +11,6 @@ export function makeTheater() {
 	canvas.className = "theater"
 
 	benevTheater.settings.addRenderer(sliders)
-
-	scene.onPointerDown = evnt => {
-		if(evnt.button === 0) engine.enterPointerlock();
-		if(evnt.button === 1) engine.exitPointerlock();
-	}
-
-	const renderer = new DepthRenderer(scene)
-	renderer.enabled = true
-
-	scene.clearColor = new Color4(62 / 255, 129 / 255, 186 / 255, 1)
-	;(<any>window).engine = engine
-	
-	// scene.performancePriority = 2
 
 	return {
 		nubContext,
